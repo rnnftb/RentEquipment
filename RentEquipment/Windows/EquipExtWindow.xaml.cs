2cd2e2b76fa72b9f31c267879dc2e1e42b34c67d
Asus <Asus@DESKTOP-GVME1K8>
2022-02-22 09:14:34 +0300

    Добавьте файлы проекта.

---
+++ RentEquipment/Windows/EquipExtWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RentEquipment.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для EquipExtWindow.xaml
+    /// </summary>
+    public partial class EquipExtWindow : Window
+    {
+        public EquipExtWindow()
+        {
+            InitializeComponent();
+            lvEquipExt.ItemsSource = ClassHelper.AppData.Context.ClientProduct.ToList();
+        }
+        private void Filter()
+        {
+            List<EF.ClientProduct> ListClientProduct = new List<EF.ClientProduct>();
+            ListClientProduct = ClassHelper.AppData.Context.ClientProduct.ToList();
+            lvEquipExt.ItemsSource = ListClientProduct;
+        }
+
+        private void lvEquipExt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete || e.Key == Key.Back)
+            {
+                try
+                {
+                    if (lvEquipExt.SelectedItem is EF.ClientProduct)
+                    {
+                        var resmsg = MessageBox.Show("Удалить запись?", "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                        if (resmsg == MessageBoxResult.No)
+                        {
+                            return;
+                        }
+                        var stf = lvEquipExt.SelectedItem as EF.ClientProduct;
+                        ClassHelper.AppData.Context.ClientProduct.Remove(stf);
+                        ClassHelper.AppData.Context.SaveChanges();
+                        MessageBox.Show("Запись успешно удалена", "Удаление", MessageBoxButton.OK, MessageBoxImage.Information);
+                        Filter();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ RentEquipment/Windows/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RentEquipment
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStaffWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.StaffWindow staffWindow = new Windows.StaffWindow();
+            this.Hide();
+            staffWindow.ShowDialog();
+            this.Show();
+        }
+
+        private void btnEquipListWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.EquipListWindow equipListWindow = new Windows.EquipListWindow();
+            this.Hide();
+            equipListWindow.ShowDialog();
+            this.Show();
+        }
+
+        private void btnEquipExtWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.EquipExtWindow equipExtWindow = new Windows.EquipExtWindow();
+            this.Hide();
+            equipExtWindow.ShowDialog();
+            this.Show();
+        }
+
+        private void btnClientList_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.ClientListWindow clientListWindow = new Windows.ClientListWindow();
+            this.Hide();
+            clientListWindow.ShowDialog();
+            this.Show();
+        }
+    }
+}
