54e34d71b933cfd61dac5160fb7ed3af5c2060e7
Никита Росляков <rnnftb@gmail.com>
2022-03-03 14:34:34 +0300

    1.1.6 - Добавлен вывод изображения и изменен вывод сотрудников

--- RentEquipment/EF/Staff.cs
+++ RentEquipment/EF/Staff.cs
@@ -30,6 +30,7 @@ namespace RentEquipment.EF
         public string Login { get; set; }
         public string Password { get; set; }
         public bool IsDeleted { get; set; }
+        public byte[] Photo { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ClientProduct> ClientProduct { get; set; }
